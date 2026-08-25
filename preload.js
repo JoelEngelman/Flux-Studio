132174c5aa51900b6211b13d922970877d70259d
Joel Engelman <joeldavidengelman@hotmail.com>
2026-08-25 17:45:00 +1000

    Expose working Electron window controls and app version

--- preload.js
+++ preload.js
@@ -1,2 +1,10 @@
-const { contextBridge } = require('electron');
-contextBridge.exposeInMainWorld('fluxStudio',{version:'0.1.0'});
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('fluxStudio', {
+  version: '0.1.1',
+  minimize: () => ipcRenderer.send('window:minimize'),
+  maximize: () => ipcRenderer.send('window:maximize'),
+  close: () => ipcRenderer.send('window:close'),
+  getVersion: () => ipcRenderer.invoke('app:version'),
+  openLatestRelease: () => ipcRenderer.send('open:github')
+});
